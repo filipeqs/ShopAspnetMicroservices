fabb5fcc121ade25ccbb74b23e050414ab82b15f
Filipe Silva <filipeqs@outlook.com>
2022-06-17 22:43:12 -0400

    get orders query

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistance/IAsyncRepository.cs
@@ -0,0 +1,23 @@
+﻿using Ordering.Domain.Comon;
+using System.Linq.Expressions;
+
+namespace Ordering.Application.Contracts.Persistance
+{
+    public interface IAsyncRepository<T> where T : EntityBase
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAsync(Expression<Predicate<T>> predicate);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Predicate<T>>? predicate = null,
+                                        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                        string? includeString = null,
+                                        bool disbleTracking = true);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Predicate<T>>? predicate = null,
+                                Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                List<Expression<Func<T, object>>>? includes = null,
+                                bool disbleTracking = true);
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrderVm>>
+    {
+
+        public GetOrdersListQuery(string userName)
+        {
+            UserName = userName;
+        }
+        public string UserName { get; set; }
+    }
+}
