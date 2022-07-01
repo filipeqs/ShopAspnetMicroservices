878867b93c4933b9ce97fdb8bc624b226a7d209c
Filipe Silva <filipeqs@outlook.com>
2022-06-30 23:02:00 -0400

    docker update

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,9 @@
-using Ordering.API.Extensions;
+using MassTransit;
 using Ordering.Application;
+using Ordering.API.Extensions;
 using Ordering.Infrastructure;
+using EventBus.Messages.Common;
+using Ordering.API.EventBusConsumer;
 using Ordering.Infrastructure.Persistance;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +17,25 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped<BasketCheckoutConsumer>();
+
+// MassTransit-RabbitMQ Configuration
+builder.Services.AddMassTransit(config =>
+{
+    config.AddConsumer<BasketCheckoutConsumer>();
+
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+
+        cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c =>
+        {
+            c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+        });
+    });
+});
 
 var app = builder.Build();
 
