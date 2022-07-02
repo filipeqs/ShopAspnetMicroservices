12c520f7590617d7d2bf52503604293b039724d8
Filipe Silva <filipeqs@outlook.com>
2022-07-02 18:26:15 -0400

    shopping aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace Shopping.Aggregator.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+                throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
+
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            return JsonSerializer.Deserialize<T>(dataAsString, options);
+        }
+    }
+}
